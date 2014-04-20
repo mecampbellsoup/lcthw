d8a271073a47db1d7a19ed3e23fe1ea931dfcb73
Matt Campbell <mecampbell25@gmail.com>
2014-04-20 19:49:02 -0400

    Ex 3

---
+++ ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+    printf("Here's the octal version of the decial number 30: %o\n", 30);
+    printf("...and here's hexadecimal for, say, 154: %x", 154);
+
+    return 0;
+}
