333b6c32d266ac9da6318c3b35bbc8b6d27031df
Matt Campbell <mecampbell25@gmail.com>
2014-04-20 18:50:35 -0400

    Ex1, ex2

---
+++ ex1.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>*/
+/*#include <stdlib.h>*/
+
+int main(int argc, char *argv[])
+{
+    /*FILE *f = fopen("fputs.txt", "w");*/
+    /*if (f == NULL)*/
+    /*{*/
+        /*printf("Error opening file!\n");*/
+        /*exit(1);*/
+    /*}*/
+
+    /*fputs("Hello world.\n", f);*/
+    /*fputs("How's it going?", f);*/
+
+    puts("Hello, world!");
+
+    return 0;
+}
